843f0e5942977c831a25e0f9777e1f2f3db986ce
mateodonino <mateodonino@gmail.com>
2024-05-09 20:05:37 -0300

    Desarrollo

--- src/components/CharacterList.jsx
+++ src/components/CharacterList.jsx
@@ -1,36 +1,36 @@
 /* eslint-disable react/prop-types */
-import { useState } from "react"
-import { CharacterCard } from "./CharacterCard"
-import { CharacterModal } from "./CharacterModal"
+import { useState } from "react";
+import { CharacterCard } from "./CharacterCard";
+import { CharacterModal } from "./CharacterModal";
 export const CharacterList = ({ list }) => {
-	const [selectedCharacter, setSelectedCharacter] = useState(null)
+  const [selectedCharacter, setSelectedCharacter] = useState(null);
 
-	return (
-		<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-			{list && list.length === 0 && (
-				<h5 className="w-full text-white font-bold text-xl text-center">
-					No hay resultados
-				</h5>
-			)}
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+      {list && list.length === 0 && (
+        <h5 className="w-full text-white font-bold text-xl text-center">
+          No hay resultados
+        </h5>
+      )}
 
-			{list &&
-				list.map((character) => (
-					<CharacterCard
-						key={character.id}
-						name={character.name}
-						image={character.image}
-						status={character.status}
-						species={character.species}
-						onClick={() => setSelectedCharacter(character)}
-					/>
-				))}
+      {list &&
+        list.map((character) => (
+          <CharacterCard
+            key={character.id}
+            name={character.name}
+            image={character.image}
+            status={character.status}
+            species={character.species}
+            onClick={() => setSelectedCharacter(character)}
+          />
+        ))}
 
-			{selectedCharacter && (
-				<CharacterModal
-					character={selectedCharacter}
-					onClick={() => setSelectedCharacter(null)}
-				/>
-			)}
-		</div>
-	)
-}
+      {selectedCharacter && (
+        <CharacterModal
+          character={selectedCharacter}
+          onClick={() => setSelectedCharacter(null)}
+        />
+      )}
+    </div>
+  );
+};
--- src/components/Skeleton.jsx
+++ src/components/Skeleton.jsx
@@ -1,5 +1,5 @@
 export const Skeleton = () => {
-	return (
-		<div className="min-w-[23vw] aspect-square bg-gray-700 rounded-lg"></div>
-	)
-}
+  return (
+    <div className="min-w-[23vw] aspect-square bg-gray-700 rounded-lg"></div>
+  );
+};
